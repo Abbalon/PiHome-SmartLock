3930caa20333395862c3e750fc024a5a1aae79ea
montbs <montbs@gmail.com>
2019-10-07 21:14:05 +0200

    # Creación de la rama local para ser ejecutada desde la RPi

--- servo.py
+++ servo.py
@@ -3,13 +3,18 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
 factory = PiGPIOFactory('192.168.10.15')
-# Seteamos el pin de datos del servoa  un puerto PWM
-servo = Servo(18, pin_factory=factory)
+# Seteamos el pin de datos del servo  un puerto PWM
+servo = Servo(17, pin_factory=factory)
 
-while True:
+print("Empezamos")
+for x in range(5):
+    print(x)
     servo.min()
     sleep(1)
     servo.mid()
     sleep(1)
     servo.max()
     sleep(1)
+    print(servo.value)
+
+print("Sha'cabo")
