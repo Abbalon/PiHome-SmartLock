30de2961c02ba235cd325ae4abeb0fa5df886541
montbs <montbs@gmail.com>
2019-10-07 20:35:06 +0200

    # Conexión con factory para acceder a los pines remotamente

--- servo.py
+++ servo.py
@@ -1,8 +1,10 @@
 from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
+factory = PiGPIOFactory('192.168.10.15')
 # Seteamos el pin de datos del servoa  un puerto PWM
-servo = Servo(18)
+servo = Servo(18, pin_factory=factory)
 
 while True:
     servo.min()
