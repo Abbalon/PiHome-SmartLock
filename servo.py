0b10bc6708d1446ecd8ee1b9688f6bd07667210b
montbs <montbs@gmail.com>
2019-10-07 21:15:55 +0200

    # Anulación del remoteFactory

--- servo.py
+++ servo.py
@@ -2,9 +2,12 @@ from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
+"""
 factory = PiGPIOFactory('192.168.10.15')
 # Seteamos el pin de datos del servo  un puerto PWM
 servo = Servo(17, pin_factory=factory)
+"""
+servo = Servo(17)
 
 print("Empezamos")
 for x in range(5):
