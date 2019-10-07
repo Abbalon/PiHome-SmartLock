2b7eeafc5c57ba06eb2852323cf88e1bba935747
montbs <montbs@gmail.com>
2019-10-07 20:55:27 +0200

    # Test no funcional del control del GPIO mediante la libreria RPi.GPIO

--- servo.py
+++ servo.py
@@ -1,8 +1,13 @@
-from gpiozero import Servo
-from gpiozero.pins.pigpio import PiGPIOFactory
+# from gpiozero import Servo
+import os
+import RPi.GPIO as GPIO
 from time import sleep
+from gpiozero.pins.pigpio import PiGPIOFactory
 
-factory = PiGPIOFactory('192.168.10.15')
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(03, GPIO.OUT)
+
+factory = PiGPIOFactory(host='192.168.10.15')
 # Seteamos el pin de datos del servoa  un puerto PWM
 servo = Servo(18, pin_factory=factory)
 
@@ -10,6 +15,7 @@ while True:
     servo.min()
     sleep(1)
     servo.mid()
+
     sleep(1)
     servo.max()
     sleep(1)
