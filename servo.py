88763d5ccef145ec2c4a6dfcf575304ae4f83da5
montbs <montbs@gmail.com>
2019-10-08 22:05:22 +0200

    Añadidos los LEDS

--- servo.py
+++ servo.py
@@ -1,22 +1,38 @@
+from gpiozero import Servo, LED
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
-from gpiozero import Servo
 
-"""
-factory = PiGPIOFactory('192.168.10.15')
+# Declaración de los pines que vamos a usar
+servo_pin = 17
+ok_led_pin = 25
+warn_led_pin = 26
+error_led_pin = 6
+monitor_led_pin = 16
+
+# factory = PiGPIOFactory(host='192.168.10.15')
 # Seteamos el pin de datos del servo  un puerto PWM
-servo = Servo(17, pin_factory=factory)
-"""
-servo = Servo(17)
+servo = Servo(servo_pin)
+# Seteo de los pines
+ok_led = LED(ok_led_pin)
+warn_led = LED(warn_led_pin)
+error_led = LED(error_led_pin)
+monitor_led = LED(monitor_led_pin)
 
 print("Empezamos")
+
 for x in range(5):
-    print(x)
+    ok_led.blink(1, 1, 5)
+    warn_led.blink(1, 1, 5)
+    error_led.blink(1, 1, 5)
+    monitor_led.blink(1, 1, 5)
+    print(servo)
     servo.min()
+    print(servo)
     sleep(1)
     servo.mid()
     sleep(1)
     servo.max()
     sleep(1)
-    print(servo.value)
+    print(x)
 
-print("Sha'cabo")
+print("e voila")
