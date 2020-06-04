099b70a88812924eeb0e2d06b3583cb35432b38c
montbs <montbs@gmail.com>
2020-06-04 18:02:45 +0200

    Actualización del script de run

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -2,4 +2,4 @@
 #Script que ejecuta el código de la puerta en local
 cd ~/Dev/Zero-Project || { echo "No se ha encontrado la ruta"; exit 1;}
 #python3 __main__.py -R False
-venv/bin/python __main__.py -R False
+venv/bin/python3 __main__.py -R False
